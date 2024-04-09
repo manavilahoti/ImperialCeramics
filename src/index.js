54c4c2c91d5e0d4a520233ed4c943aa434a085aa
Manavi Lahoti <manavilahoti07@gmail.com>
2024-04-10 01:50:14 +0530

    added navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './components/Navbar.css';
+import './components/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
