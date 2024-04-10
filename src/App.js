0e8f0b8f0d4e0b93eac318044b85fb18814aacb6
Manavi Lahoti <manavilahoti07@gmail.com>
2024-04-11 02:59:49 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -5,22 +5,21 @@ import Clients from "./components/pages/Clients"
 import Contact from "./components/pages/Contact"
 import Gallery from "./components/pages/Gallery"
 import Products from "./components/pages/Products"
-import "./App.css";
-import {Routes, Route} from "react-router-dom";
+//import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className="banner"></div>
       <Routes>
-        <Route path= "/about" element = {<About />} />
-        <Route path= "/home" element = {<Home />} />
-        <Route path= "/clients" element = {<Clients />} />
-        <Route path= "/contact" element = {<Contact />} />
-        <Route path= "/gallery" element = {<Gallery />} />
-        <Route path= "/products" element = {<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,86 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from "react-router-dom";
-function Navbar() {
-    const [sticky, setSticky] = useState(false);
-    useEffect(()=>{
-        const handleScroll = () => {
-            setSticky(window.scrollY> 0);
-            console.log(window.scrollY);
-    }
-        window.addEventListener('scroll', handleScroll)
-        return () => window.removeEventListener('scroll', handleScroll)
-    });
-    return (
-        <nav className={`${sticky ? "sticky" : "" }`}>
-            <div className='nav-inner'> 
-                <span className='logo'>
-                    ICPL
-                </span>
-                <div className='links'>
-                    <ul>
-                        <li><Link to = "/home">Home</Link> </li>
-                        <li><Link to = "/about">About</Link></li>
-                        <li><Link to = "/products">Products</Link> </li>
-                        <li><Link to = "/gallery">Gallery</Link> </li>
-                        <li><Link to = "/clients">Clients</Link> </li>
-                        <li><Link to = "/contact">Contact us</Link> </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+    { name: 'Home', href: '/', current: true },
+    { name: 'About', href: '/about', current: false },
+    { name: 'Products', href: '/products', current: false },
+    { name: 'Gallery', href: '/gallery', current: false },
+    { name: 'Clients', href: '/clients', current: false },
+    { name: 'Contact', href: '/contact', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
-export default Navbar;
+
+export default function Example() {
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-9xl px-1 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+                
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+
+
+                {/* Mobile menu button*/}
+
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+
+              </div>
+              <div className="flex flex-auto items-start justify-start sm:items-stretch sm:justify-end">
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-5">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
