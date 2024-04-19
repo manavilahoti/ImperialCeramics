256c5035c761cb00687040014e7a48dff78ac4e2
Manavi Lahoti <manavilahoti07@gmail.com>
2024-04-19 15:26:56 +0530

    clients

--- src/components/pages/Clients.jsx
+++ src/components/pages/Clients.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
-import c1 from '/Users/manavilahoti/Desktop/demo/src/images/c1.jpeg';
-import c2 from '/Users/manavilahoti/Desktop/demo/src/images/c2.jpeg';
-import c3 from '/Users/manavilahoti/Desktop/demo/src/images/c3.jpeg';
-import c4 from '/Users/manavilahoti/Desktop/demo/src/images/c4.jpeg';
-import c5 from '/Users/manavilahoti/Desktop/demo/src/images/c5.jpeg';
-import c6 from '/Users/manavilahoti/Desktop/demo/src/images/c6.jpeg';
-import c7 from '/Users/manavilahoti/Desktop/demo/src/images/c7.jpeg';
-import c8 from '/Users/manavilahoti/Desktop/demo/src/images/c8.jpeg';
-import c9 from '/Users/manavilahoti/Desktop/demo/src/images/c9.jpeg';
-import c10 from '/Users/manavilahoti/Desktop/demo/src/images/c10.jpeg';
-import c11 from '/Users/manavilahoti/Desktop/demo/src/images/c11.jpeg';
-import c12 from '/Users/manavilahoti/Desktop/demo/src/images/c12.jpeg';
-import c13 from '/Users/manavilahoti/Desktop/demo/src/images/c13.jpeg';
-import c14 from '/Users/manavilahoti/Desktop/demo/src/images/c14.jpeg';
-import c15 from '/Users/manavilahoti/Desktop/demo/src/images/c15.jpeg';
+import cl1 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl1.jpeg';
+import cl2 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl2.png';
+import cl3 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl3.webp';
+import cl4 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl4.jpeg';
+import cl5 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl5.avif';
+import cl6 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl6.jpeg';
+import cl7 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl7.png';
+import cl8 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl8.jpeg';
+import cl9 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl9.png';
+import cl10 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl10.jpeg';
+import cl11 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl11.jpeg';
+import cl12 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl12.webp';
+import cl13 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl13.jpeg';
+import cl14 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl14.gif';
+import cl15 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl15.png';
+import cl16 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl16.png';
+import cl17 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl17.png';
+import cl18 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl18.jpeg';
+import cl19 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl19.jpg';
+import cl20 from '/Users/manavilahoti/Desktop/demo/src/images/clients/cl20.jpeg';
 
 export default function DefaultGallery() {
   const data = [
-    { imageLink: c1 },
-    { imageLink: c2 },
-    { imageLink: c3 },
-    { imageLink: c4 },
-    { imageLink: c5 },
-    { imageLink: c6 },
-    { imageLink: c7 },
-    { imageLink: c8 },
-    { imageLink: c9 },
-    { imageLink: c11 },
-    { imageLink: c13 },
-    { imageLink: c14 },
-    { imageLink: c15 },
-    { imageLink: c10 },
-    { imageLink: c12 },
+    { imageLink: cl17 },
+    { imageLink: cl2 },
+    { imageLink: cl15 },
+    { imageLink: cl20 },
+    { imageLink: cl10 },
+    { imageLink: cl6 },
+    { imageLink: cl7 },
+    { imageLink: cl8 },
+    { imageLink: cl9 },
+    { imageLink: cl11 },
+    { imageLink: cl13 },
+    { imageLink: cl14 },
+    { imageLink: cl16 },
+    { imageLink: cl5 },
+    { imageLink: cl12 },
+    { imageLink: cl3 },
+    { imageLink: cl1 },
+    { imageLink: cl18 },
+    { imageLink: cl19 },
+    { imageLink: cl4},
   ];
 
   return (
@@ -41,11 +51,13 @@ export default function DefaultGallery() {
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-7">
           {data.map(({ imageLink }, index) => (
             <div key={index} className="p-2">
-              <img
-                className="w-32 h-32 sm:w-48 sm:h-48 md:w-auto md:h-auto rounded-lg object-center"
-                src={imageLink}
-                alt={`gallery-photo-${index}`}
-              />
+              <div className="aspect-square">
+                <img
+                  className="object-cover object-center rounded-lg"
+                  src={imageLink}
+                  alt={`gallery-photo-${index}`}
+                />
+              </div>
             </div>
           ))}
         </div>
