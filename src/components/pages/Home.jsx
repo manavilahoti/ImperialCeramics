d9b741685dff9471baebc6a02ddbfb79d2a4dd70
Manavi Lahoti <manavilahoti07@gmail.com>
2024-05-08 03:01:04 +0530

    home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -22,15 +22,24 @@ export function Home() {
   };
 
   return (
-    <div className="px-4 py-8">
-      <div className="max-w-4xl mx-auto">
+    <div className=" ">
+      <div className=" ">
         {/* Carousel */}
-        <div className="relative mb-12 bg-white rounded-lg shadow-xl overflow-hidden">
-          <button className="absolute left-0 top-1/2  -translate-y-1/2 px-4 py-2 rounded-r-full bg-gray-800 text-white" onClick={previousImage}>
+        <div className="relative mb-12 bg-white shadow-xl overflow-hidden w-full max-h-120">
+          <button className="absolute left-0 top-1/2 transform -translate-y-1/2 px-4 py-2 rounded-r-full bg-gray-800 text-white" onClick={previousImage}>
             &lt;
           </button>
-          <img src={images[currentImageIndex]} alt={`Image ${currentImageIndex + 1}`} className="w-full object-contain" />
-          <button className="absolute right-0 top-1/2  -translate-y-1/2 px-4 py-2 rounded-l-full bg-gray-800 text-white" onClick={nextImage}>
+          <div className="flex">
+            {images.map((image, index) => {
+              const adjustedIndex = (currentImageIndex + index) % images.length;
+              return (
+                <div key={index} className="w-1/3 flex-shrink-0 mr-4" style={{ display: index >= 0 && index < 3 ? 'block' : 'none', marginTop: '20px' }}>
+                  <img src={images[adjustedIndex]} alt={`Image ${adjustedIndex + 1}`} className="w-full h-full" style={{ marginTop: '20px' }} />
+                </div>
+              );
+            })}
+          </div>
+          <button className="absolute right-0 top-1/2 transform -translate-y-1/2 px-4 py-2 rounded-l-full bg-gray-800 text-white" onClick={nextImage}>
             &gt;
           </button>
         </div>
